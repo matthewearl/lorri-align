4dd480629afee1a009897caa2f9b98b2b49e9d46
Matthew Earl <matthew.earl@gmail.com>
2015-08-09 09:42:23 +0100

    Fix imports and whitespace issues in stack.py

--- stack.py
+++ stack.py
@@ -25,10 +25,14 @@ Routines for stacking registered images.
 """
 
 __all__ = (
+    'get_bounding_rect',
     'StackedImage',
 )
 
 import collections
+
+import cv2
+import numpy
 
 class _BoundingRect(collections.namedtuple('_Rect', ('x', 'y', 'w', 'h'))):
     """
@@ -45,14 +49,12 @@ class _BoundingRect(collections.namedtuple('_Rect', ('x', 'y', 'w', 'h'))):
                              top_left[1, 0],
                              bottom_right[0, 0] - top_left[0, 0],
                              bottom_right[1, 0] - top_left[1, 0])
-        
 
     def expand(self, points):
         """Expand the bounding rect with a matrix of points."""
         points = numpy.vstack(numpy.matrix(points), self.corners)
 
         return self.from_points(points)
-
 
     @property
     def corners(self):
@@ -82,7 +84,7 @@ def get_bounding_rect(ims_and_transforms):
             reference coordinate frame into the image's coordinate frame.
 
     """
-    
+
     def im_corners(im):
         return numpy.matrix([0, 0],
                             [0, im.shape[0]],
@@ -139,11 +141,11 @@ class StackedImage(object):
     def im(self):
         return self._im
 
-    def add_image(im, M):
+    def add_image(self, im, M):
         cv2.warpAffine(im,
                        M * _translate_matrix(-self._rect.corners[:, 0]),
                        self._rect.size,
                        dst=self._im,
                        borderMode=cv2.BORDER_TRANSPARENT,
                        flags=cv2.WARP_INVERSE_MAP)
-                     
+
